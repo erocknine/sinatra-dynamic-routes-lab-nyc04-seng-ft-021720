58992a5d8b662432ad1a3964cdd8295ec7ebde40
Sir Eric <echeungsk@gmail.com>
2020-03-09 21:39:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,14 +13,14 @@ class App < Sinatra::Base
   end
   
   get '/say/:number/:phrase' do
-    params[:number].to_i * params[:phrase]
+     response = params[:number].to_i * params[:phrase]
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    '#{params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5]'
+    '#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}'
   end
   
   get '/:operation/:number1/:number2' do
- 
+    
   end
 end

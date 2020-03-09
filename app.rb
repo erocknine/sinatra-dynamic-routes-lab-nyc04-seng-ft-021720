0ef7a5623588327a731225c6b96d33bbad1116be
Sir Eric <echeungsk@gmail.com>
2020-03-09 21:37:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,6 +17,9 @@ class App < Sinatra::Base
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
+    five_words = ''
+    
+    
     @fiveWords = params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5]
     "#{@fiveWords}"
   end

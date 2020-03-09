39e99c6645e077cc97b18e13a609fb4681b1293a
Sir Eric <echeungsk@gmail.com>
2020-03-09 21:34:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,8 +13,8 @@ class App < Sinatra::Base
   end
   
   get '/say/:number/:phrase' do
-    @phrase = params[:number] * params[:num]
-    "#{@phrase}"
+    
+    
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
